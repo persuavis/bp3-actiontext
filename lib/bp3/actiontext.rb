f2b2ed357d4231597e0eac332d118fcc6dec8b6b
Wim den Braven <wimdenbraven@persuavis.com>
2024-01-08 15:03:00 -0800

    Initial commit

---
+++ lib/bp3/actiontext.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'actiontext/version'
+
+module Bp3
+  module Actiontext
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
